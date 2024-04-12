2fcb64c38dc6fb2a58f913c3d9924e0f38f4db12
kenta.yamada <kenta.yamada@fox-hound.jp>
2024-04-12 13:07:45 +0900

    fix : コンテンツページのタイトル文字と幅を修正

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -8,9 +8,9 @@ export const metadata: Metadata = {
 
 export default async function Todo() {
   return (
-    <div className={'card mx-32 mt-6 w-auto bg-base-100 p-6 shadow-xl'}>
+    <div className={'card mx-16 mt-6 w-auto bg-base-100 p-6 shadow-xl'}>
       <h1 className={'mb-6 text-center text-4xl font-extrabold tracking-wider'}>
-        TODO Page
+        TODO List
       </h1>
       <TodoContents></TodoContents>
     </div>
